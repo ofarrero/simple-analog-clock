8d5dac52b81cbc627d7825149fc5f9986798cbb1
ofarrero <66687614+ofarrero@users.noreply.github.com>
2020-06-20 12:49:45 +0100

    static clock face with correct positioning

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+
+var date = new DATE();
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = (hr*360/12) + (min(360/60)/12); //extra bit here is so that the hand moves as the minutes go by so that it doesnt suddenly jump to next number
+let minPosition = (min*360/60)+ (sec*(360/60)/60) ; //extra bit for same reason as above
+let secPosition = sec*360/60;
+
+HOURHAND.style.transform = "rotate("+ hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate("+ minPosition + "deg)";
+SECONDHAND.style.transform = "rotate("+ secPosition + "deg)";
