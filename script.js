94ec1e94266e700fc2b4fd4c38322e6d4056c1c3
ofarrero <66687614+ofarrero@users.noreply.github.com>
2020-06-20 13:04:58 +0100

    clock face updates every second, fixed errors  on line 7 and 14

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
+function runClock(){
+  var date = new Date();
+  console.log(date);
 
-var date = new DATE();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
 
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
+  let hrPosition = (hr*360/12) + (min*(360/60)/12); //extra bit here is so that the hand moves as the minutes go by so that it doesnt suddenly jump to next number
+  let minPosition = (min*360/60)+ (sec*(360/60)/60) ; //extra bit for same reason as above
+  let secPosition = sec*360/60;
 
-let hrPosition = (hr*360/12) + (min(360/60)/12); //extra bit here is so that the hand moves as the minutes go by so that it doesnt suddenly jump to next number
-let minPosition = (min*360/60)+ (sec*(360/60)/60) ; //extra bit for same reason as above
-let secPosition = sec*360/60;
+  HOURHAND.style.transform = "rotate("+ hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate("+ minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate("+ secPosition + "deg)";
+}
 
-HOURHAND.style.transform = "rotate("+ hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate("+ minPosition + "deg)";
-SECONDHAND.style.transform = "rotate("+ secPosition + "deg)";
+var interval = setInterval(runClock, 1000); //setInterval() will run function every x milliseconds
