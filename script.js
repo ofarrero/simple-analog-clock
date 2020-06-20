5369d4177cf8d2fff5642220727b9df89699fa56
ofarrero <66687614+ofarrero@users.noreply.github.com>
2020-06-20 13:16:34 +0100

    stopped second hand from going backwards at the top of every minute

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
-function runClock(){
+
   var date = new Date();
   console.log(date);
 
@@ -14,6 +14,12 @@ function runClock(){
   let hrPosition = (hr*360/12) + (min*(360/60)/12); //extra bit here is so that the hand moves as the minutes go by so that it doesnt suddenly jump to next number
   let minPosition = (min*360/60)+ (sec*(360/60)/60) ; //extra bit for same reason as above
   let secPosition = sec*360/60;
+
+function runClock(){
+
+  hrPosition = hrPosition + (3/360); //this makes sure the clock is on the right time as we are not updating with date
+  minPosition = minPosition + (1/10);
+  secPosition = secPosition + 6;
 
   HOURHAND.style.transform = "rotate("+ hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate("+ minPosition + "deg)";
